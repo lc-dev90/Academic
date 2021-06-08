a0ce4bd66d7de3bcfdd41fbcaa4bf2b7df2701b2
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-08 00:29:55 -0300

    fetching data from api

---
+++ Photops/app.js
@@ -0,0 +1,29 @@
+const keyAuth = "563492ad6f91700001000001eb455511155d448bbb49848d9904e578";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-button");
+
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: keyAuth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src="${photo.src.large}">  </img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
