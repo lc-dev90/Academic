d23e15d9b8363d85abaa0091e88c1bac63c5286f
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-08 01:27:30 -0300

    search functionality

--- Photops/app.js
+++ Photops/app.js
@@ -1,9 +1,19 @@
 const keyAuth = "563492ad6f91700001000001eb455511155d448bbb49848d9904e578";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-button");
-
+const form = document.querySelector(".search-form");
 let searchValue;
+
+// listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
@@ -26,4 +36,24 @@ async function curatedPhotos() {
   });
 }
 
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: keyAuth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src="${photo.src.large}">  </img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
 curatedPhotos();
