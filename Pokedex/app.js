9c9af2268504f9f56e3176a025b71f85985196dd
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-08 23:43:12 -0300

    fetching data from api

---
+++ Pokedex/app.js
@@ -0,0 +1,37 @@
+const fetchPokemon = () => {
+  const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  const pokemonPromises = [];
+
+  for (let i = 1; i <= 150; i++) {
+    pokemonPromises.push(
+      fetch(getPokemonUrl(i)).then((response) => response.json())
+    );
+  }
+
+  Promise.all(pokemonPromises).then((pokemons) => {
+    /* console.log(pokemons); */
+
+    const lisPokemons = pokemons.reduce((acumulator, pokemon) => {
+      const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
+
+      acumulator += `
+      <li class="card ${types[0]}">
+      <img class="card-image" alt="${
+        pokemon.name
+      }" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-subtitle">${pokemon.types
+          .map((typeInfo) => typeInfo.type.name)
+          .join(" | ")}</p>
+      </li>`;
+      return acumulator;
+    }, "");
+
+    const ul = document.querySelector('[data-js="pokedex"]');
+
+    ul.innerHTML = lisPokemons;
+  });
+};
+
+fetchPokemon();
