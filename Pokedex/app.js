4396b7a55f36d35b7988cd21b3b830ff95aa5900
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-09 00:00:01 -0300

    getting out function generatePokemonPromises

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -1,11 +1,12 @@
 const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const generatePokemonPromises = () =>
+  Array(150)
+    .fill()
+    .map((_, index) =>
+      fetch(getPokemonUrl(index + 1)).then((response) => response.json())
+    );
+
 const fetchPokemon = () => {
-  const generatePokemonPromises = () =>
-    Array(150)
-      .fill()
-      .map((_, index) =>
-        fetch(getPokemonUrl(index + 1)).then((response) => response.json())
-      );
   const pokemonPromises = generatePokemonPromises();
 
   Promise.all(pokemonPromises).then((pokemons) => {
