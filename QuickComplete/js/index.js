e6a1e70d9884a3d69bdf7102f066d38109b1d097
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-10 19:31:41 -0300

    search function

---
+++ QuickComplete/js/index.js
@@ -0,0 +1,19 @@
+const search = document.getElementById("search");
+const matchList = document.getElementById("match-list");
+
+const searchStates = async (searchText) => {
+  const res = await fetch("../data/states.json");
+  const states = await res.json();
+
+  let matches = states.filter((state) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return state.name.match(regex) || state.abbr.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+  }
+  console.log(matches);
+};
+
+search.addEventListener("input", () => searchStates(search.value));
