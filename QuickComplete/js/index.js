7e2a06f36c6b3ffbd53e4bb8d6923d33feb823a0
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-10 19:45:46 -0300

    search functionality ok

--- QuickComplete/js/index.js
+++ QuickComplete/js/index.js
@@ -12,8 +12,27 @@ const searchStates = async (searchText) => {
 
   if (searchText.length === 0) {
     matches = [];
+    matchList.innerHTML = "";
   }
-  console.log(matches);
+  outputHtml(matches);
+};
+
+const outputHtml = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+    <div class="card card-body mb-1">
+      <h4>${match.name} (${match.abbr}) 
+        <span class="text-primary">${match.capital}</span>
+      </h4>
+      <small>Lat: ${match.lat} / Long: ${match.long}</small>
+    </div>
+    `
+      )
+      .join("");
+    matchList.innerHTML = html;
+  }
 };
 
 search.addEventListener("input", () => searchStates(search.value));
