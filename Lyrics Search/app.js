780020199ed1941eb336a61862185faba84ade4d
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-10 23:57:26 -0300

    map

--- Lyrics Search/app.js
+++ Lyrics Search/app.js
@@ -5,12 +5,24 @@ const prevAndNextContainer = document.querySelector("#prev-and-next-container");
 
 const apiURL = "https://api.lyrics.ovh";
 
-const fetchSongs = (term) => {
-  fetch(`${apiURL}/suggest/${term}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
+const insertSongsIntoPage = (songsInfo) => {
+  songsContainer.innerHTML = songsInfo.data
+    .map(
+      (song) => `
+    <li class="song">
+      <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>  
+      <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+    </li>
+    `
+    )
+    .join("");
+};
+
+const fetchSongs = async (term) => {
+  const response = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await response.json();
+
+  insertSongsIntoPage(data);
 };
 
 form.addEventListener("submit", (event) => {
