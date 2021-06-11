feb3d9f8b397953283159d20e5546145debb3972
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-10 23:20:42 -0300

    fetching api

---
+++ Lyrics Search/app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#search");
+const songsContainer = document.querySelector("#songs-container");
+const prevAndNextContainer = document.querySelector("#prev-and-next-container");
+
+const apiURL = "https://api.lyrics.ovh";
+
+const fetchSongs = (term) => {
+  fetch(`${apiURL}/suggest/${term}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchTerm = searchInput.value.trim();
+  if (!searchTerm) {
+    songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo válido.</li>`;
+    return;
+  }
+  fetchSongs(searchTerm);
+});
