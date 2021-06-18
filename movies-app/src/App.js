73d7feb46935061f33b0653c7c6f66c4e5e9c8e6
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-17 21:21:08 -0300

    api keys and end points

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Movie from "./components/Movie";
 
+/* "https://api.themoviedb.org/3/movie/550?api_key=3b48dbd14d25d9323dfacec549beb683" */
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3b48dbd14d25d9323dfacec549beb683";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=3b48dbd14d25d9323dfacec549beb683&query=";
+
+const IMG_API = "https://images.tmdb.org/t/p/w1280";
+
 function App() {
   const movies = ["1", "2", "3"];
   return (
