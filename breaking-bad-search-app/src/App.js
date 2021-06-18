600ebb720ca1cc5823cc3e2834d2ef0a427885f8
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-18 03:03:17 -0300

    pagination

--- breaking-bad-search-app/src/App.js
+++ breaking-bad-search-app/src/App.js
@@ -4,11 +4,20 @@ import axios from "axios";
 import Header from "./components/ui/Header";
 import CharacterGrid from "./components/characters/CharacterGrid";
 import Search from "./components/ui/Search";
+import Pagination from "./components/ui/Pagination";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState("");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(20);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = items.slice(indexOfFirstPost, indexOfLastPost);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -21,11 +30,19 @@ const App = () => {
     fetchItems();
   }, [query]);
 
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="container">
       <Header />
       <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid isLoading={isLoading} items={items} />
+      <CharacterGrid isLoading={isLoading} items={currentPosts} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={items.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
