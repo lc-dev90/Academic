96adbd63ec775665e9a70b4650a748324f9b00da
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-17 22:01:18 -0300

    useEffect fetching data from api

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
-/* "https://api.themoviedb.org/3/movie/550?api_key=3b48dbd14d25d9323dfacec549beb683" */
 const FEATURED_API =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3b48dbd14d25d9323dfacec549beb683";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3b48dbd14d25d9323dfacec549beb683&page=1";
 
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?&api_key=3b48dbd14d25d9323dfacec549beb683&query=";
 
-const IMG_API = "https://images.tmdb.org/t/p/w1280";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 function App() {
-  const movies = ["1", "2", "3"];
-  return (
-    <div className="App">
-      {movies.map((movie) => (
-        <Movie />
-      ))}
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((responseMovies) => responseMovies.json())
+      .then((moviesJson) => {
+        console.log(moviesJson);
+        setMovies(moviesJson.results);
+      });
+  }, []);
+
+  return <div>{movies.length > 0 && movies.map((movie) => <Movie />)}</div>;
 }
 
 export default App;
