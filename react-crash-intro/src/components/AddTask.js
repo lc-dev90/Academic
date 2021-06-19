cf77d4172b455e56f156866f4b3daffe832b6c11
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-19 09:02:57 -0300

    hooks addContact

--- react-crash-intro/src/components/AddTask.js
+++ react-crash-intro/src/components/AddTask.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTask = () => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className="add-form">
       <div className="form-control">
         <label htmlFor="task">Task</label>
-        <input type="text" id="task" placeholder="Add Task" />
+        <input
+          type="text"
+          id="task"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="dayAndTime">Day & Time</label>
-        <input type="text" id="dayAndTime" placeholder="Add Day & Time" />
+        <input
+          type="text"
+          id="dayAndTime"
+          placeholder="Add Day & Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
       <div className="form-control form-control-check">
         <label htmlFor="reminder">Set Reminder</label>
-        <input type="checkbox" id="reminder" />
+        <input
+          type="checkbox"
+          id="reminder"
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
       <input className="btn btn-block" type="submit" value="Save Task" />
     </form>
