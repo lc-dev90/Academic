a3e5c7cc5976e2bf0c4e61d2f46c7ae93a0c65be
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-19 09:36:18 -0300

    change color button close

--- react-crash-intro/src/App.js
+++ react-crash-intro/src/App.js
@@ -50,7 +50,10 @@ function App() {
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTaks(!showAddTask)} />
+      <Header
+        onAdd={() => setShowAddTaks(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
