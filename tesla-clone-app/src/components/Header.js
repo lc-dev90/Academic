4d2cd274cbe799df8eb44b6d7fbdb516be8704d6
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-19 23:09:20 -0300

    header links navbar

--- tesla-clone-app/src/components/Header.js
+++ tesla-clone-app/src/components/Header.js
@@ -7,10 +7,44 @@ const Header = () => {
       <a href="#">
         <img src="/images/logo.svg" alt="Logo" />
       </a>
+      <MenuGroup>
+        <div>Model S</div>
+        <div>Model X</div>
+        <div>Model Y</div>
+      </MenuGroup>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
     </Container>
   );
 };
 
 export default Header;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  min-height: 60px;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 20px;
+`;
+
+const MenuGroup = styled.div`
+  display: flex;
+  text-transform: uppercase;
+
+  div {
+    padding: 0 10px;
+    cursor: pointer;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding-right: 10px;
+  }
+`;
