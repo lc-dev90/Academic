034434db6faedff0446d2ec1cea6788ec1f94a93
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-20 07:04:25 -0300

    status close hamburguer

--- tesla-clone-app/src/components/Header.js
+++ tesla-clone-app/src/components/Header.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Header = () => {
+  const [burguerStatus, setBurguerStatus] = useState(false);
   return (
     <Container>
       <a href="#">
@@ -16,10 +18,42 @@ const Header = () => {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <MenuIconContainer>
+        <MenuIconContainer onClick={(e) => setBurguerStatus(!burguerStatus)}>
           <MenuIcon />
         </MenuIconContainer>
       </RightMenu>
+      <BurguerNav status={burguerStatus}>
+        <CloseContainer onClick={() => setBurguerStatus(!burguerStatus)}>
+          <Close />
+        </CloseContainer>
+        <li>
+          <a href="#">Model S</a>
+        </li>
+        <li>
+          <a href="#">Model Y</a>
+        </li>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade In</a>
+        </li>
+        <li>
+          <a href="#">Cyber Truck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+      </BurguerNav>
     </Container>
   );
 };
@@ -64,4 +98,34 @@ const MenuIconContainer = styled.div`
   cursor: pointer;
   animation-name: animate-down;
   animation-duration: 10s;
+`;
+
+const Close = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  z-index: 1;
+`;
+
+const BurguerNav = styled.div`
+  position: fixed;
+  width: 300px;
+  background: white;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px;
+  list-style: none;
+  transform: ${(props) =>
+    props.status ? "translateX(0)" : "translateX(100%)"};
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
 `;
