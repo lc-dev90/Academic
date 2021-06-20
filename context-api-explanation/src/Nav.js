451e96547aecad0c5e327a35060154c2883aec1e
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-20 08:52:57 -0300

    nav

---
+++ context-api-explanation/src/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "black",
+        color: "white",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 100px",
+        maxWidth: "100%",
+      }}
+    >
+      <h3>Luiz</h3>
+      <p>List of movies: </p>
+    </div>
+  );
+};
+
+export default Nav;
