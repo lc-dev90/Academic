f7668bf947df5bdb3f7408252afbf03c326448d0
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-21 11:45:13 -0300

    routes

--- react-router-explanation/src/App.js
+++ react-router-explanation/src/App.js
@@ -1,13 +1,31 @@
 import "./App.css";
 import Nav from "./Nav";
+import About from "./About";
+import Shop from "./Shop";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
 
 export default App;
--- react-router-explanation/src/Nav.js
+++ react-router-explanation/src/Nav.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav>
-      <h3>Logo</h3>
-      <ul>
-        <li>About</li>
-        <li>Shop</li>
+      <Link to="/">
+        <h3>Logo</h3>
+      </Link>
+      <ul className="nav-links">
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/shop">
+          <li>Shop</li>
+        </Link>
       </ul>
     </nav>
   );
