ade4c6f11b8dbc3be787eff44e53ee8041437626
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-21 16:09:16 -0300

    basic explanation

--- redux-explanation/src/index.js
+++ redux-explanation/src/index.js
@@ -2,6 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+
+// store globalized state
+
+// action increment
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// display it in console
+store.subscribe(() => console.log(store.getState()));
+
+// dispatch
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
