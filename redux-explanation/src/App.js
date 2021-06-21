62d3f90dfd7f2063c11985cf875d1101201894d5
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-21 19:52:30 -0300

    example redux

--- redux-explanation/src/App.js
+++ redux-explanation/src/App.js
@@ -1,5 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { increment, decrement } from "./actions";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <p>This is a valuable information</p> : ""}
+    </div>
+  );
 }
 
 export default App;
