06b5760dbf79a1b0ffce569c0c7fe18bc235c903
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-22 05:39:25 -0300

    navbar styled

--- disney-site-clone/src/components/Header.js
+++ disney-site-clone/src/components/Header.js
@@ -7,10 +7,31 @@ const Header = () => {
       <Logo src="/images/logo.svg" />
       <NavMenu>
         <a>
-          <img src="images/home-icon.svg" />
+          <img src="images/home-icon.svg" alt="home" />
           <span>HOME</span>
         </a>
+        <a>
+          <img src="images/search-icon.svg" alt="search" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="images/watchlist-icon.svg" alt="watchlist" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="images/original-icon.svg" alt="original" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="images/movie-icon.svg" alt="movies" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="images/series-icon.svg" alt="serie" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
+      <UserImg src="https://pbs.twimg.com/profile_images/1393509631197163524/6UqNQddM_400x400.jpg" />
     </Nav>
   );
 };
@@ -29,4 +50,29 @@ const Logo = styled.img`
   width: 80px;
 `;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+  align-items: center;
+  a {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  max-width: 100%;
+  height: 48px;
+  width: 48px;
+  border-radius: 50%;
+`;
