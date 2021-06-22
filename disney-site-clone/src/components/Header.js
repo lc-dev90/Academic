d244647acc3b04bfd44706138c31505374a5b9ee
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-21 22:05:18 -0300

    nav bar - styled components

---
+++ disney-site-clone/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
+};
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div``;
