77786d41b1180efb69b209d460c1ae13860e6ade
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-23 18:05:37 -0300

    search input component styled

--- anime-search-app/src/App.js
+++ anime-search-app/src/App.js
@@ -1,11 +1,31 @@
+import React, { useState } from "react";
+import SearchInput from "./SearchInput";
 import "./App.css";
 
+import styled from "styled-components";
+
 function App() {
+  const [text, setText] = useState("");
+  console.log(text);
   return (
     <div className="App">
-      <h1>Animes</h1>
+      <Header>
+        <h1>Animes</h1>
+        <SearchInput value={text} onChange={(e) => setText(e)} />
+      </Header>
     </div>
   );
 }
 
 export default App;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 40px;
+  h1 {
+    margin-bottom: 20px;
+    letter-spacing: 1.4px;
+  }
+`;
--- anime-search-app/src/SearchInput.js
+++ anime-search-app/src/SearchInput.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-const SearchInput = () => {
-  return <div>Search Input</div>;
+const SearchInput = ({ value, onChange }) => {
+  return (
+    <Search
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
+
+const Search = styled.input`
+  padding: 10px;
+  font-size: 1.2rem;
+  border-radius: 5px;
+  outline: none;
+  border: 1px solid black;
+  width: 450px;
+  max-width: 95%;
+`;
