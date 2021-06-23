7b07a66462cf60c9207ea2d7cc88bf66e4a4fe88
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-23 18:39:29 -0300

    fetching data from api using useeffect

--- anime-search-app/src/App.js
+++ anime-search-app/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchInput from "./SearchInput";
 import "./App.css";
 
 import styled from "styled-components";
 
 function App() {
+  const [info, setInfo] = useState({});
   const [text, setText] = useState("");
+
+  useEffect(() => {
+    if (text) {
+      const fetchData = async () => {
+        const response = await fetch(
+          `https://kitsu.io/api/edge/anime?filter[text]=${text}`
+        );
+        const data = await response.json();
+        setInfo(data);
+      };
+      fetchData();
+    }
+  }, [text]);
   console.log(text);
   return (
     <div className="App">
@@ -13,6 +27,13 @@ function App() {
         <h1>Animes</h1>
         <SearchInput value={text} onChange={(e) => setText(e)} />
       </Header>
+      {info.data && (
+        <ul>
+          {info.data.map((anime) => (
+            <li key={anime.id}>{anime.attributes.canonicalTitle}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
