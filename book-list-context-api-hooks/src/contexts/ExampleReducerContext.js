88d5794383ca60701cd1d13cbc5268f62c922224
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-27 22:08:37 -0300

    example useReducer with context api

---
+++ book-list-context-api-hooks/src/contexts/ExampleReducerContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+const AgeContext = createContext();
+
+const AgeContextProvider = (props) => {
+  const [age, setAge] = useState(20);
+
+  const addOneToAge = () => setAge(age + 1);
+  const addFiveToAge = () => setAge(age + 5);
+  const addNumToAge = (num) => setAge(age + num);
+
+  return (
+    <AgeContext.Provider value={(age, addOneToAge, addFiveToAge, addNumToAge)}>
+      {props.children}
+    </AgeContext.Provider>
+  );
+};
