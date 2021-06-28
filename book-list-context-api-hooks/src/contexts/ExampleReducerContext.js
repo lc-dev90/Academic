134692a9955752883c7bcf5f08494e4f6d520d7f
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-27 22:17:57 -0300

    useReducer example

--- book-list-context-api-hooks/src/contexts/ExampleReducerContext.js
+++ book-list-context-api-hooks/src/contexts/ExampleReducerContext.js
@@ -1,16 +1,33 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 
 const AgeContext = createContext();
 
-const AgeContextProvider = (props) => {
-  const [age, setAge] = useState(20);
+const ageReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ONE":
+      return state + 1;
+    case "ADD_FIVE":
+      return state + 5;
+    case "ADD_NUM":
+      return state + action.num;
+    default:
+      return state;
+  }
+};
 
-  const addOneToAge = () => setAge(age + 1);
+dispatch({ type: "ADD_FIVE" });
+dispatch({ type: "ADD_NUM", num: 12 });
+
+const AgeContextProvider = (props) => {
+  /* const [age, setAge] = useState(20); */
+  const [age, dispatch] = useReducer(ageReducer, 20);
+
+  /*  const addOneToAge = () => setAge(age + 1);
   const addFiveToAge = () => setAge(age + 5);
-  const addNumToAge = (num) => setAge(age + num);
+  const addNumToAge = (num) => setAge(age + num); */
 
   return (
-    <AgeContext.Provider value={(age, addOneToAge, addFiveToAge, addNumToAge)}>
+    <AgeContext.Provider value={(age, dispatch)}>
       {props.children}
     </AgeContext.Provider>
   );
