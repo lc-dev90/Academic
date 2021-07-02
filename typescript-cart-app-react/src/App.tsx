93a70f626c5ffb600c963d727b97ba5fed8925d8
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-01 22:52:45 -0300

    fetching data

--- typescript-cart-app-react/src/App.tsx
+++ typescript-cart-app-react/src/App.tsx
@@ -1,4 +1,19 @@
-import "./App.css";
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+//Components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+
+//Styles
+import { Wrapper } from "./App.styles";
+
+const getProducts = async () => {
+  await (await fetch("https://fakestoreapi.com/products")).json();
+};
 
 const App = () => {
   return (
