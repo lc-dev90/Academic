528cffc4a0badc1c7c72b1f888952558db9c1175
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-03 13:52:47 -0300

    reducer

--- react-redux-cart-explanation/src/App.js
+++ react-redux-cart-explanation/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 
+import Header from "./containers/Header";
+
 const App = () => {
   return (
     <div className="App">
       <h1>Hello wolrd!</h1>
+      <Header />
     </div>
   );
 };
