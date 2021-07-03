1d3be5bc27dc4b382889cbdb4caf7a568d2c7147
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-03 13:56:57 -0300

    routes

--- react-redux-cart-explanation/src/App.js
+++ react-redux-cart-explanation/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello wolrd!</h1>
-      <Header />
+      <Router>
+        <Header />
+        <Route path="/" exact component={ProductListing} />
+        <Route path="/product/:productId" exact component={ProductDetail} />
+        <Route>404 not Found!</Route>
+      </Router>
     </div>
   );
 };
