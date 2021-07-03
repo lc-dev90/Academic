5f5ac384a332abbe112ea2b72dd9d8f62feed431
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-03 11:37:04 -0300

    created products form structure

--- typescript-redux-react-cart-app/src/Products/ProductForm.tsx
+++ typescript-redux-react-cart-app/src/Products/ProductForm.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-interface ProductFormProps {
-
-}
+interface ProductFormProps {}
 
 const ProductForm: React.FC<ProductFormProps> = ({}) => {
-  return ()
-}
+  return (
+    <>
+      <h2>Add Game To The Store</h2>
+      <form>
+        <input type="text" placeholder="Game title" name="title" />
+        <input type="number" placeholder="Price" name="price" />
+        <input type="text" placeholder="Id" name="id" />
+        <button>Add Game</button>
+      </form>
+    </>
+  );
+};
 
-export default ProductForm
+export default ProductForm;
