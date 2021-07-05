76062536167d94a64af190a4130902faf355b8a1
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-04 22:13:08 -0300

    create homescreen structure and initial styles

--- mern-react-redux-shopping-cart/frontend/src/screens/HomeScreen.js
+++ mern-react-redux-shopping-cart/frontend/src/screens/HomeScreen.js
@@ -1,9 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+
+//components
+import Product from "../components/Product";
+
 const HomeScreen = () => {
-  return <Home>Hello from homescreen</Home>;
+  return (
+    <Home>
+      <h2>Latest product</h2>
+      <div className="products">
+        <Product />
+      </div>
+    </Home>
+  );
 };
 
 export default HomeScreen;
 
-const Home = styled.div``;
+const Home = styled.div`
+  max-width: 1300px;
+  margin: 1rem auto;
+  h2 {
+    font-size: 1.5rem;
+    color: #171717;
+    margin-bottom: 1rem;
+    margin-left: 8px;
+  }
+  .products {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
