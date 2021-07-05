4f2626c512deb920ea47d6e718c2e77fa83dc153
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-05 14:01:08 -0300

    remove from cart

--- mern-react-redux-shopping-cart/frontend/src/redux/actions/cartActions.js
+++ mern-react-redux-shopping-cart/frontend/src/redux/actions/cartActions.js
@@ -18,3 +18,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: actionTypes.REMOVE_FROM_CART,
+    payload: id,
+  });
+
+  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+};
