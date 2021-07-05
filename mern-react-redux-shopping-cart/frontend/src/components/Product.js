03a0475bc285e62190b70051ba902000a8dd7b8e
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-04 22:33:53 -0300

    product view

--- mern-react-redux-shopping-cart/frontend/src/components/Product.js
+++ mern-react-redux-shopping-cart/frontend/src/components/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Product = () => {
-  return <div>hello from product</div>;
+  return (
+    <Prod>
+      <img src="" alt=""></img>
+      <div>
+        <p>Product 1</p>
+        <p>
+          Lorem ipsun dolor natate sfsare adforsal gorlisk mulitapete smam
+          tiluski numipo retokil nanadal retemo
+        </p>
+        <p>$ 499.99</p>
+        <Link to={`/product/${1111}`}> View</Link>
+      </div>
+    </Prod>
+  );
 };
 
 export default Product;
+
+const Prod = styled.div``;
