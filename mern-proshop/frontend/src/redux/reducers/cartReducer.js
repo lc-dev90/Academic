bbb63506e9f137750525e020d05a29b81aa0f59b
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-09 13:05:05 -0300

    added cart functionality and remove from cart

--- mern-proshop/frontend/src/redux/reducers/cartReducer.js
+++ mern-proshop/frontend/src/redux/reducers/cartReducer.js
@@ -4,9 +4,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-
       const existItem = state.cartItems.find((x) => x.product === item.product);
-
       if (existItem) {
         return {
           ...state,
@@ -20,6 +18,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         };
       }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
     default:
       return state;
   }
