1fa1ef659c91bdc563ebf9f6bcf4bd7e7ea54dc5
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-16 22:47:49 -0300

    useMemo

--- react-hooks-tutorial/src/pages/UseMemo.js
+++ react-hooks-tutorial/src/pages/UseMemo.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState, useMemo, useEffect } from "react";
+
+import styled from "styled-components";
 
 const UseMemo = () => {
-  return <div>Welcome to UseMemo Hook Page</div>;
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => slowFunction(number), [number]);
+
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+
+  useEffect(() => {
+    console.log("Theme changed");
+  }, [themeStyles]);
+  return (
+    <Container>
+      <h1>Welcome to UseMemo Hook Page</h1>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      ></input>
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </Container>
+  );
 };
 
+function slowFunction(num) {
+  console.log("Calling Slow Function");
+  for (let i = 0; i < 1000000000; i++) {}
+  return num * 2;
+}
+
 export default UseMemo;
+
+const Container = styled.div`
+  padding: 30px;
+  width: 100%;
+  font-size: 2rem;
+  input {
+    padding: 20px;
+    display: block;
+    font-size: 2rem;
+  }
+  button {
+    padding: 20px;
+    display: block;
+    font-size: 2rem;
+  }
+  h2 {
+    margin-bottom: 30px;
+  }
+  div {
+    font-size: 2rem;
+    padding: 20px;
+    button {
+      padding: 20px;
+      font-size: 2rem;
+      cursor: pointer;
+    }
+  }
+`;
--- react-hooks-tutorial/src/pages/UseRef.js
+++ react-hooks-tutorial/src/pages/UseRef.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const UseRef = () => {
-  return <div>Welcome to UseRef Hook Page</div>;
+  const [name, setName] = useState("");
+  return (
+    <Container>
+      <h2>Welcome to UseRef Hook Page</h2>
+      <div>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="name"
+        ></input>
+        <p>My name is: {name}</p>
+      </div>
+    </Container>
+  );
 };
 
 export default UseRef;
+const Container = styled.div`
+  padding: 30px;
+  width: 100%;
+  h2 {
+    margin-bottom: 30px;
+  }
+  div {
+    font-size: 2rem;
+    input {
+      padding: 20px;
+      font-size: 2rem;
+      margin-bottom: 10px;
+      border: 1px solid black;
+    }
+    button {
+      padding: 20px;
+      font-size: 2rem;
+      cursor: pointer;
+    }
+  }
+`;
