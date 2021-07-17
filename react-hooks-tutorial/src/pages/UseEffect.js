21a180e0f675ae2b4174bcbb2c87288b8c8265a9
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-16 22:15:08 -0300

    useffect

--- react-hooks-tutorial/src/pages/UseEffect.js
+++ react-hooks-tutorial/src/pages/UseEffect.js
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 const UseEffect = () => {
-  const [resourceType, setResourceType] = useState("posts");
+  /* const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then((response) => response.json())
+    .then((json) => setItems(json));
+    /* const [resourceType, setResourceType] = useState("posts");
+  }, [resourceType]); */
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Container>
       <h1>WELCOME TO USEEFFECT HOOKS PAGE!</h1>
-      <div>
+      <div>{windowWidth}</div>
+      {/* <div>
         <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("users")}>Users</button>
         <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h2>{resourceType}</h2>
+      {items.map((item) => (
+        <pre key={item.id}>{JSON.stringify(item)}</pre>
+      ))} */}
     </Container>
   );
 };
